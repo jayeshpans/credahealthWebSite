3356fea65ff94cb82272e2428e8abc288f682ea9
jpansare <jayesh.pansare@credahealth.com>
2023-06-09 12:00:42 +0530

    validate provider page header section

---
+++ src/stepDefinition/ProviderStepDef.java
@@ -0,0 +1,32 @@
+package stepDefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ProviderStepDef {
+    @Given("^click on provider menu$")
+    public void click_on_provider_menu() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^validate provider page title$")
+    public void validate_provider_page_title() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^validate Patient Communication section as \"([^\"]*)\"$")
+    public void validate_Patient_Communication_section_as(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^validate Patient Communication section img as \"([^\"]*)\"$")
+    public void validate_Patient_Communication_section_img_as(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
